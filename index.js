5364c9924db3251c089aec3592c9e74367de2c16
lukeschunk <lukeschunk@gmail.com>
2015-12-02 16:41:23 -0700

    pushing for zach

--- index.js
+++ index.js
@@ -9,17 +9,17 @@ app.get('/', function (req, res) {
 });
 
 io.on('connection', function (socket) {
-    socket.on('clientSaid', function (message) { // event listener
-        if (message.target === 'luke') {
-            io.emit('serverSaid', message.msg); //     
-        } else {
-            console.log('message: ' + message.msg);
-        }
+      socket.on('clientSaid', function (message) { // event listener
+          if (message.target === 'luke') {
+              io.emit('serverSaid', message.msg); //
+          } else {
+              console.log('message: ' + message.msg);
+          }
 
-    });
-    socket.on('chatted', function (ourSomeMessage) {
-        console.log("we received:" + ourSomeMessage);
-    });
+      });
+      socket.on('chatted', function (ourSomeMessage) {
+          console.log("we received:" + ourSomeMessage);
+      });
 });
 
 http.listen(3000, function () { // event listener too
